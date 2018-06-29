aa04cf1242e7cc9757a5a098934249e2db731c2f
Arunsahu42 <arunsahu44@gmail.com>
2018-06-29 10:44:49 +0530

    python directory changed

---
+++ python/pname.py
@@ -0,0 +1,3 @@
+def welcome(name):
+	print "Hello! ",name
+	return
---
+++ python/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+counter =100;
+miles=1000.0
+name = "john"
+
+print counter
+print miles
+print name
+
+print 3/2 < 5-7
+
+f=lambda x, y:x+y
+
